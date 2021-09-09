716832a294102ac01547dd92ed1fcc4503afa7e7
Elijah_Coleman <ecoleman2@eagles.bridgewater.edu>
2021-09-09 01:53:58 -0400

    Added comments to help explain

--- Blackjack.java
+++ Blackjack.java
@@ -13,6 +13,7 @@ class Blackjack{
 	ArrayList<String> deck = new ArrayList<String>();
 	ArrayList<Integer> value = new ArrayList<Integer>();
 	ArrayList<Integer> shuffle = new ArrayList<Integer>();
+	//loop for adding values to value arrray
 	for(int i=0;i<4;i++){
 		int v=2;
 		for(int x=0;x<10;x++){
@@ -22,13 +23,12 @@ class Blackjack{
 			value.add(10);
 		}
 	}
+	//loop for creating shuffle array
 	for(int i=0;i<51;i++){
 		shuffle.add(i);
 	}
-//	for(int i=0;i<value.size();i++){
-//		System.out.println(value.get(i));}
-// For checkin Value Arraylist
 	
+	//a bunch of loops for each suite in order to populate the string array
 	int c = 2;
 	for(int i=0;i<9;i++){
 		deck.add(c+" of Clubs");
@@ -69,9 +69,6 @@ class Blackjack{
 	deck.add("Queen of Spades");
 	deck.add("King of Spades");
 
-//	for(int i=0;i<deck.size();i++){
-//		System.out.println(deck.get(i));}
-// For checking Deck  arrayList
 
 	System.out.println("Welcome to BlackJack!");
 	System.out.println("House Rule #1: Ace=11 unless you bust when you draw it, if that's the case Ace=1.");
@@ -86,19 +83,20 @@ while(playAgain){
 	while(newHand){ 
 	Thread.sleep(1000);
 	System.out.println("Your two cards are");
-	
+	//suffles the collection array
 	Collections.shuffle(shuffle);
 	
-		
+	//int card is equal to 0, use that to pull the new index 0 from the shuffled shuffle array and use whatever the int number at that index is to call from the deck and value array	
 	player = value.get(shuffle.get(card));
 	Thread.sleep(1000);
 	System.out.println(deck.get(shuffle.get(card)));	
-	
+	//int card now equals 1 so it will pull the random shuffled int at index 1 and use that to call from deck and value 
 	card ++;
 	
 	Thread.sleep(1000);
 	System.out.println(deck.get(shuffle.get(card)));
 	player=((player)+(value.get(shuffle.get(card))));
+	//if statement to make sure if the Ace(11) makes the player bust it becomes Ace(1)
 	if(value.get(shuffle.get(card))==11&&player>21){
 		player=player-10;
 	}
@@ -117,6 +115,7 @@ while(playAgain){
 	System.out.println("Would you like to Hit (h) or Stand (s)?");
 	String hitOrStand = scan.nextLine();
 	
+	//a bunch of if statements for deciding if you want to hit or stand. all statements are inside of each other that way if you decide to stand it skips the rest of the if statements
 	if (hitOrStand.equalsIgnoreCase("h")){
 		card++;
 		Thread.sleep(1000);
@@ -213,7 +212,7 @@ while(playAgain){
 	
 	Thread.sleep(1000);
 	System.out.println("The Dealer now has "+dealer);
-	
+//loop for dealer to run in order to draw cards until they reach 17 or bust	
 	while(dealer<17){
 		card++;
 
@@ -229,7 +228,7 @@ while(playAgain){
 	}
 	break;
 
-}
+}//after you break out of the first while loop you end up here, this has the outcomes depending on the values of player and dealer
 	if(dealer>21){
 		Thread.sleep(1000);
 		System.out.println("Since the Dealer has "+dealer+" they bust, you Win!");
@@ -261,10 +260,12 @@ while(playAgain){
 		System.out.println("Since you have "+player+" you bust, The Dealer Wins!");
 	}
 	System.out.println("You now have $"+bet);
+	//checks if you have enough money to keep playing
 	if (bet<=0){
 		System.out.println("You're all out of money.");
 	break;
 	}
+	//if you enter n then the inital while loop that contains the second while loop will close and you will exit the program
 	System.out.println("Would you like to play again? Yes (y), No (n).");
 	String replay = scan.nextLine();
 	if(replay.equalsIgnoreCase("n")){
